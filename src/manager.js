1a399e73869afd24077f3ef5ddbce625a851fdad
Jason Lenoble <jason.lenoble@gmail.com>
2018-02-21 18:17:49 +0100

    Read data from files

--- src/manager.js
+++ src/manager.js
@@ -1,25 +1,47 @@
+import fs from 'fs';
 import TestWriter from 'testwriter';
 
 export default class Manager {
-  constructor ({name, data}) {
+  constructor ({title, data}) {
+    let _data = data;
+
+    if (typeof data === 'string') {
+      _data = {};
+      fs.readFileSync(data).toString().split('\n').forEach(line => {
+        if (line) {
+          _data[line] = undefined;
+        }
+      });
+    }
+
     Object.defineProperties(this, {
-      name: {
-        value: name,
+      title: {
+        value: title,
+      },
+
+      data: {
+        value: data,
       },
 
       writer: {
-        value: new TestWriter(data),
+        value: new TestWriter(_data),
       },
     });
   }
 
   run () {
-    this.writer.defineTests([
-      `All ${this.name}s`,
-      function (key) {
-        return key;
-      },
-      function () {},
-    ], 1, describe, it);
+    try {
+      this.writer.defineTests([
+        this.title,
+        function (key) {
+          return key;
+        },
+        function () {},
+      ], 1, describe, it);
+    } catch (err) {
+      if (err.message.includes('No descriptions provided for tests')) {
+        console.warn(`No data in ${this.data}`);
+      }
+    }
   }
 }
