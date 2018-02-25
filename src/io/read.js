2d50fdaa7462c10f89ba7a27476361db584a726d
Jason Lenoble <jason.lenoble@gmail.com>
2018-02-25 02:36:07 +0100

    Added async read, renamed sync read

--- src/io/read.js
+++ src/io/read.js
@@ -1,9 +1,23 @@
-import fs from 'fs';
+import fse from 'fs-extra';
 
-export function read (filename, func) {
-  const data = {};
+export async function read (filename, func) {
+  let data = {};
+  const buffer = await fse.readFile(filename);
 
-  fs.readFileSync(filename).toString().split('\n').forEach(line => {
+  buffer.toString().split('\n').forEach(line => {
+    if (line) {
+      data[line] = typeof func === 'function' ? func(line): func;
+    }
+  });
+
+  return data;
+}
+
+export function readSync (filename, func) {
+  let data = {};
+  const buffer = fse.readFileSync(filename);
+
+  buffer.toString().split('\n').forEach(line => {
     if (line) {
       data[line] = typeof func === 'function' ? func(line): func;
     }
