b7f6b0b884e2cd889a6dcb42c0c29882ccb35a83
Jason Lenoble <jason.lenoble@gmail.com>
2018-02-27 01:28:07 +0100

    Full refactoring
    Removed class.
    All functions are now static.
    Have the UI loaded dynamically from DB.

--- src/manager.js
+++ src/manager.js
@@ -1,90 +1,108 @@
 import TestWriter from 'testwriter';
-import db, {dbUri, readFactory, createFactory, deleteFactory} from './db';
+import {uiFile} from './config';
+import db, {dbUri, UI, CRUD} from './db';
+import {readJSONSync, writeJSON} from './io';
 
-export default class Manager {
-  constructor ({title} = {}) {
-    if (!title) {
-      throw new Error('You must provide a valid title to the manager');
+const ui = {};
+const Manager = {};
+
+export default Manager;
+
+const map = {
+  add: CRUD.createFactory,
+  find: CRUD.readFactory,
+  update: CRUD.updateFactory,
+  remove: CRUD.deleteFactory,
+};
+
+['add', 'find', 'update', 'remove'].forEach(key => {
+  Manager[key] = function ({
+    Model, title, data, describe, it,
+    itFunc = function (key) {
+      return key;
+    },
+    beforeFunc = function () {},
+    afterFunc = function () {},
+  }) {
+    const func = map[key](Model);
+    return Manager.run({
+      title, data, describe, it, itFunc, beforeFunc, afterFunc,
+      doFunc: func,
+    });
+  };
+});
+
+Manager.fetchUI = async function () {
+  const abort = err => {
+    if (db.connection) {
+      db.connection.close();
     }
+    throw err;
+  };
 
-    Object.defineProperty(this, 'title', {
-      value: title,
+  try {
+    await db.connect(dbUri);
+    const data = await UI.find();
+
+    writeJSON(uiFile, data);
+
+    data.forEach(({name, title}) => {
+      ui[name] = title;
+    });
+
+    return db.connection.close();
+  } catch (err) {
+    abort(err);
+  }
+};
+
+Manager.preloadUI = function () {
+  if (!Object.keys(ui).length) {
+    ui.greeting = 'Welcome';
+
+    readJSONSync(uiFile).forEach(({name, title}) => {
+      ui[name] = title;
     });
   }
+};
 
-  run ({
-    title, data, arity = 1, describe, it,
-    itFunc = function (key) {
-      return key;
-    },
-    doFunc = function () {},
-    beforeFunc = function () {},
-    afterFunc = function () {},
-  }) {
-    describe(this.title, function () {
-      before(function () {
-        // eslint-disable-next-line no-invalid-this
-        return db.connect(dbUri).then(beforeFunc.bind(this));
-      });
+Manager.initUI = function () {
+  if (!Object.keys(ui).length) {
+    Manager.preloadUI();
+    Manager.fetchUI();
+  }
+};
 
-      after(async function () {
-        // eslint-disable-next-line no-invalid-this
-        return db.connection.close().then(afterFunc.bind(this));
-      });
+Manager.run = function ({
+  title, data, arity = 1, describe, it,
+  itFunc = function (key) {
+    return key;
+  },
+  doFunc = function () {},
+  beforeFunc = function () {},
+  afterFunc = function () {},
+}) {
+  Manager.initUI();
 
-      try {
-        const funcs = title ? [title, itFunc, doFunc] : [itFunc, doFunc];
-        new TestWriter(data).defineTests(funcs, arity, describe, it);
-      } catch (err) {
-        if (err.message.includes('No descriptions provided for tests')) {
-          return; // Ok to have no input
-        }
-        throw err;
+  describe(ui.greeting || 'Welcome', function () {
+    before(function () {
+      // eslint-disable-next-line no-invalid-this
+      return db.connect(dbUri).then(beforeFunc.bind(this));
+    });
+
+    after(function () {
+      // eslint-disable-next-line no-invalid-this
+      return db.connection.close().then(afterFunc.bind(this));
+    });
+
+    try {
+      const funcs = title ? [title, itFunc, doFunc] : [itFunc, doFunc];
+      new TestWriter(data).defineTests(funcs, arity, describe, it);
+    } catch (err) {
+      if (err.message.includes('No descriptions provided for tests')) {
+        return; // Ok to have no input
       }
-    });
-  }
-
-  add ({
-    Model, title, data, describe, it,
-    itFunc = function (key) {
-      return key;
-    },
-    beforeFunc = function () {},
-    afterFunc = function () {},
-  }) {
-    return this.run({
-      title, data, describe, it, itFunc, beforeFunc, afterFunc,
-      doFunc: createFactory(Model),
-    });
-  }
-
-  remove ({
-    Model, title, data, describe, it,
-    itFunc = function (key) {
-      return key;
-    },
-    beforeFunc = function () {},
-    afterFunc = function () {},
-  }) {
-    return this.run({
-      title, data, describe, it, itFunc, beforeFunc, afterFunc,
-      doFunc: deleteFactory(Model),
-    });
-  }
-
-  find ({
-    Model, title, data, describe, it,
-    itFunc = function (key) {
-      return key;
-    },
-    beforeFunc = function () {},
-    afterFunc = function () {},
-  }) {
-    return this.run({
-      title, data, describe, it, itFunc, beforeFunc, afterFunc,
-      doFunc: readFactory(Model),
-    });
-  }
-}
-
-Manager.UI = {};
+      throw err;
+    }
+  });
+};
