eac2eea38534d3730b591146136b3ab9773489bd
Jason Lenoble <jason.lenoble@gmail.com>
2018-02-27 01:19:59 +0100

    Added paths for uiFile and lastQueryFile

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
+import path from 'path';
+
 export const host = 'localhost';
 export const db = 'organic';
-
 export const mongoUri = `mongodb://${host}/${db}`;
+
+export const lastQueryFile = path.join(process.cwd(),
+  'test/private/auto/last-query.json');
+export const uiFile = path.join(process.cwd(), 'test/private/auto/ui.json');
