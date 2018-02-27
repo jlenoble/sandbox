79551c52b796f186ea47ec6a8a3b98b9b0a41ee8
Jason Lenoble <jason.lenoble@gmail.com>
2018-02-27 01:26:33 +0100

    added 'name' field
    Forced collection name as 'UI'

--- src/db/models/ui.js
+++ src/db/models/ui.js
@@ -1,7 +1,10 @@
 import mongoose from '../mongoose';
 
 const uiSchema = new mongoose.Schema({
+  name: String,
   title: String,
+}, {
+  collection: 'UI',
 });
 
 export const UI = mongoose.model('UI', uiSchema);
